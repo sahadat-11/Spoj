fb958e7abf59e10cbf675c68b0ebf131dd914d6d
sahadat-11 <137852614+sahadat-11@users.noreply.github.com>
2023-10-19 21:27:33 +0600

    addded

--- Totient Extreme.cpp
+++ Totient Extreme.cpp
@@ -2,10 +2,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define ull unsigned long long
+#define ll long long
 const int N = 1e4 + 7;
 int phi[N];
-ull prephi[N];
+ll prephi[N];
 int main() {
 	ios_base::sync_with_stdio(0);
     cin.tie(0);
@@ -20,9 +20,9 @@ int main() {
     	}
     }
     //for(int i = 1; i <= 20; i++) cout << phi[i] << "\n";
-    prephi[0] = (ull)0;
+    prephi[0] = (ll)0;
     for(int i = 1; i <= N; i++) {
-    	prephi[i] = prephi[i - 1] + (ull)phi[i];
+    	prephi[i] = prephi[i - 1] + (ll)phi[i];
     }
     int t; cin >> t;
     while(t--) {
@@ -31,3 +31,4 @@ int main() {
     }
     return 0;
 }
+// https://prnt.sc/FDGG0qsT-sSO
